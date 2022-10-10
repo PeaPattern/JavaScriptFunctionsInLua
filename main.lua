802cd5c328583486d9dbfb822100fd87ba2319c0
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2022-10-10 16:19:37 -0400

    fixes + js.try
    end -- ok if u actually watch changes i didnt test this soooo dont blame on me if it doesnt work

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 local JavaScript = {}
 
 function JavaScript.startsWith(String: string?, Start: string?, Position: number?)
-    if not String or type(String) ~= "string" then error("<startsWith> String is not string.") end
-    if not Start or type(Start) ~= "string" then error("<startsWith> Start is not string.") end
-    if not Position then Position = 1 elseif type(Position) ~= "number" then error("<startsWith> Position is not number.") end
+    if not String or typeof(String) ~= "string" then error("<startsWith> String is not string.") end
+    if not Start or typeof(Start) ~= "string" then error("<startsWith> Start is not string.") end
+    if not Position then Position = 1 elseif typeof(Position) ~= "number" then error("<startsWith> Position is not number.") end
     
     if String:sub(Position, Start:len()+Position-1) == Start then
         return true
@@ -13,15 +13,25 @@ function JavaScript.startsWith(String: string?, Start: string?, Position: number
 end
 
 function JavaScript.fill(Table: table?, Fill: number?, From: number?, To: number?)
-    if not Table or type(Table) ~= "table" then error("<fill> Table is not a table.") end
-    if not Fill or type(Fill) ~= "number" then error("<fill> Fill is not number.") end
-    if not From then From = 1 elseif type(From) ~= "number" then error("<fill> From is not number.") end
-    if not To then To = #Table elseif type(To) ~= "number" then error("<fill> To is not number.") end
+    if not Table or typeof(Table) ~= "table" then error("<fill> Table is not a table.") end
+    if not Fill or typeof(Fill) ~= "number" then error("<fill> Fill is not number.") end
+    if not From then From = 1 elseif typeof(From) ~= "number" then error("<fill> From is not number.") end
+    if not To then To = #Table elseif typeof(To) ~= "number" then error("<fill> To is not number.") end
     
     for Iterate = 0, To - From, 1 do
         Table[Iterate + From] = Fill
     end
     return Table
+end
+
+function JavaScript.try(Try, Catch)
+    if not Try or typeof(Try) ~= "function" then error("<try> Try is not function.") end
+    if not Catch then Catch = function() elseif typeof(Catch) ~= "function" then error("<try> Catch is not function.") end
+    
+    local success, error = pcall(Try)
+    if not success then
+        Catch(error)
+    end -- ok if u actually watch changes i didnt test this soooo dont blame on me if it doesnt work
 end
 
 return JavaScript
